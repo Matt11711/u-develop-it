71991f4269626e8a1cf0dd4d1c0e201ebcb76675
Matt11711 <matthew.d.allen97@gmail.com>
2022-04-28 16:15:20 -0400

    setup server and database connections

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mysql = require('mysql2');
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: process.env.DB_USER,
+      // Your MySQL password
+      password:process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
